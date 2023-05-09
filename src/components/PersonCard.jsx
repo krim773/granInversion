b5d483fd892a9e556943fc65bb94719c6220c7b8
krim773 <ezbenitez24@gmail.com>
2023-05-09 02:31:11 +0000

    first commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+//todo lo invertido estara debajo para la comparativa
+const PersonCard = ({lastName, firstName, age, haircolor}) => {
+  return (
+    <div>
+        <h2>{lastName}, {''} {firstName} </h2>
+        <p>Age: {''} {age} </p>
+        <p>Hair color: {''} {haircolor} </p>
+    </div>
+  )
+}
+
+export default PersonCard
+
+//de aqui en adelante estara el antiguo codigo de la tarea "todoJunto"
+
+// import React, { useState } from 'react';
+
+// const PersonCard = (props) => {
+//     //desestructurar de los props y crear nuevas constantes
+//     //con diferencia a la tarea "apoyalo" en este caso al desestructurar se cambiara la edad a "edadInicial" para usar en el useState
+//     const { firstName, lastName, age: edadInicial, hairColor } = props;
+
+//     //se utilizara 
+//     const [age, setAge] = useState(edadInicial)
+
+//     const cumple = () => {
+//         setAge( parseInt(age) + 1)
+//     }
+//     return (
+//         // en este div se renderizaran los datos con los respectvos ordenes dados en la tarea de "apoyalos"
+//         <div>
+//             <h2>{lastName},{''} {firstName}</h2>
+//             <p>Age: {''}{age}</p>
+//             <p>Hair Color: {''} {hairColor}</p>
+//             <button onClick={cumple}>Birthday Button for {firstName} {lastName}! </button>
+//         </div>
+//     );
+// };
+
+// export default PersonCard;
